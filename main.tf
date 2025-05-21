276d8e801878364ee955039cc1467b8ede7081e2
venu101502-gopal <venuajet101502@gmail.com>
2025-05-21 20:05:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -173,37 +173,3 @@ resource "aws_msk_cluster" "msk_cluster" {
   ]
 }
 
-output "kafka_client_public_dns" {
-  description = "Public DNS of the Kafka client EC2 instance"
-  value       = aws_instance.kafka_client_ec2.public_dns
-}
-
-output "msk_cluster_arn" {
-  description = "ARN of the MSK cluster"
-  value       = aws_msk_cluster.msk_cluster.arn
-}
-
-output "vpc_id" {
-  description = "The VPC ID"
-  value       = var.vpc_id
-}
-
-output "public_subnet_id" {
-  description = "Public Subnet ID"
-  value       = aws_subnet.public_subnet.id
-}
-
-output "private_subnet_one_id" {
-  description = "Private Subnet One ID"
-  value       = aws_subnet.private_subnet_one.id
-}
-
-output "private_subnet_two_id" {
-  description = "Private Subnet Two ID"
-  value       = aws_subnet.private_subnet_two.id
-}
-
-output "private_subnet_three_id" {
-  description = "Private Subnet Three ID"
-  value       = aws_subnet.private_subnet_three.id
-}
