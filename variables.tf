bcf0197b694ec62f0e23a5e122d2e64856473031
venu101502-gopal <venuajet101502@gmail.com>
2025-05-21 20:02:37 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,32 +1,50 @@
 variable "region" {
   description = "AWS region to create resources"
+  type        = string
   default     = "us-east-1"
 }
 
 variable "vpc_id" {
   description = "The VPC ID where the subnets will be created"
+  type        = string
 }
 
 variable "key_name" {
   description = "The name of the EC2 Key Pair"
+  type        = string
 }
 
-variable "availability_zone" {
-  description = "The availability zone to launch resources in"
+variable "availability_zone_1" {
+  description = "Availability Zone for private subnet 1"
+  type        = string
+}
+
+variable "availability_zone_2" {
+  description = "Availability Zone for private subnet 2"
+  type        = string
+}
+
+variable "availability_zone_3" {
+  description = "Availability Zone for private subnet 3"
+  type        = string
 }
 
 variable "public_subnet_cidr" {
   description = "CIDR block for the public subnet"
+  type        = string
 }
 
 variable "private_subnet_one_cidr" {
   description = "CIDR block for the first private subnet"
+  type        = string
 }
 
 variable "private_subnet_two_cidr" {
   description = "CIDR block for the second private subnet"
+  type        = string
 }
 
 variable "private_subnet_three_cidr" {
   description = "CIDR block for the third private subnet"
+  type        = string
 }
